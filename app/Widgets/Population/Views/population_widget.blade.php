e2c00c4500345b76fe30202c9533485d6a147ff7
Codemis <johnathan@pulos.com>
2020-09-17 10:52:05 -0700

    Adding back the uppercase directories.

---
+++ app/Widgets/Population/Views/population_widget.blade.php
@@ -0,0 +1,11 @@
+<h2>Population</h2>
+<dl>
+    <dt>Total</dt>
+    <dd>{{ number_format($population->total_population) }}</dd>
+    <dt>Men</dt>
+    <dd>{{ number_format($population->population_men) }}</dd>
+    <dt>Women</dt>
+    <dd>{{ number_format($population->population_women) }}</dd>
+    <dt>Density</dt>
+    <dd>{{ number_format($population->population_density) }}</dd>
+</dl>
