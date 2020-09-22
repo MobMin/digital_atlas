e2bafeaccce2b73fdf6a8d49a5a54f868b4ddc28
Codemis <johnathan@pulos.com>
2020-09-21 18:27:30 -0700

    Forgot to add translation files.

---
+++ app/Widgets/JoshuaProject/Translations/en/widget.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * This file is part of Digital Atlas.
+ *
+ * Digital Atlas is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Year of Prayer API is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+/**
+ * Localization strings for the widget.
+ */
+return [
+    'people_groups'         =>  'people groups',
+    'religion'              =>  'religion',
+    'main_religion'         =>  'main religion',
+    'total'                 =>  'total',
+    'total_unreached'       =>  'total unreached',
+    '1040_window'           =>  'in 10/40 window?',
+    'yes'                   =>  'yes',
+    'no'                    =>  'no',
+    'percentages'           =>  'percentages',
+    'buddhism'              =>  'buddhism',
+    'christian'             =>  'christian',
+    'evangelical'           =>  'evangelical',
+    'hinduism'              =>  'hinduism',
+    'islam'                 =>  'islam',
+    'ethnic'                =>  'ethnic',
+    'other'                 =>  'other',
+    'nonreligious'          =>  'non-religious',
+    'provided_by'           =>  'Data provided by',
+    'joshua_project'        =>  'Joshua Project',
+];
