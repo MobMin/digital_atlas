2f52fa96046b174bbba4e3b8b6b0141bbd057221
Codemis <johnathan@pulos.com>
2020-09-23 15:21:49 -0700

    Added template for new widget.

---
+++ app/Widgets/InternetUsage/InternetUsageServiceProvider.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * This file is part of Digital Atlas.
+ *
+ * Digital Atlas is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Digital Atlas is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+namespace App\Widgets\InternetUsage;
+
+use Illuminate\Support\ServiceProvider;
+
+class InternetUsageServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $ds = DIRECTORY_SEPARATOR;
+        $this->loadMigrationsFrom(__DIR__ . $ds . 'Migrations');
+        $this->loadViewsFrom(__DIR__ . $ds . 'Views', 'internet-usage');
+        $this->loadTranslationsFrom(__DIR__ . $ds . 'Translations', 'internet-usage');
+        $this->publishes([
+            __DIR__  . $ds . 'Config.php' => config_path('widgets' . $ds . 'internet_usage.php'),
+        ]);
+        $this->callAfterResolving(Schedule::class, function (Schedule $schedule) {
+            // Schedule your cron
+        });
+        if ($this->app->runningInConsole()) {
+            // Add commands for artisan here
+            // $this->commands([]);
+        }
+    }
+}
---
+++ app/Widgets/InternetUsage/Views/internet_usage_widget.blade.php
@@ -0,0 +1,8 @@
+<div class="card m-3">
+    <div class="card-body">
+        <h3 class="card-title">{{ trans('internet-usage::widget.title') }}</h3>
+    </div>
+    <div class="card-footer text-muted">
+        <p class="credit">{{ ucfirst(trans('internet-usage::widget.provided_by')) }} <a href="https://data.worldbank.org/indicator/IT.NET.USER.ZS" target="_blank" rel="nofollow">{{ trans('internet-usage::widget.provider') }}</a></a></p>
+    </div>
+</div>
