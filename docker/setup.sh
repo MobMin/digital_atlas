a9e1c636743f60e015f25540280721110805e460
Codemis <johnathan@pulos.com>
2020-09-30 10:46:48 -0700

    Removed unused returns.

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,12 +2,12 @@
 # This is a handy script for installing the project using docker.  This will set up the
 # development environment for you.
 
-echo "Setting up the dev environment.\r\n"
+echo "Setting up the dev environment."
 docker-compose up -d --build
 docker-compose run --rm da_composer install
 docker-compose run --rm da_npm install
 docker-compose run --rm da_npm run dev
 docker-compose run --rm da_artisan migrate
 docker-compose run --rm da_artisan db:seed
-echo "Set up completed. Please visit http://localhost:8080/ in your browser.\r\n"
-echo "Happy Coding!\r\n"
+echo "Set up completed. Please visit http://localhost:8080/ in your browser."
+echo "Happy Coding!"
