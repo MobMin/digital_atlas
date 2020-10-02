94ae507b87112cb11634b889757283afdc4b6bd4
Codemis <johnathan@pulos.com>
2020-10-01 17:41:09 -0700

    Wrote a test for the generator.

---
+++ tests/Unit/WidgetGeneratorTest.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * This file is part of Digital Atlas.
+ *
+ * Digital Atlas is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Digital Atlas is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @author Johnathan Pulos <johnathan@missionaldigerati.org>
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+namespace Tests\Unit;
+
+/**
+ * Generator uses the wrong test case
+ * @link https://github.com/laravel/framework/issues/34209
+ */
+use Tests\TestCase;
+
+class WidgetGeneratorTest extends TestCase
+{
+    /**
+     * You are not allowed to generate a widget with the same name.
+     *
+     * @return void
+     */
+    public function testNoDuplicateWidgets()
+    {
+        $this->artisan('project:make:widget Population')
+            ->expectsOutput('That widget name already exists!')
+            ->assertExitCode(0);
+    }
+}
