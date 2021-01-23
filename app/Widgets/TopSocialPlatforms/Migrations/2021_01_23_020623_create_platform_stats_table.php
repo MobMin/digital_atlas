1d2138ae2f73f81b5e93bda72d419a684807bcae
Codemis <johnathan@pulos.com>
2021-01-22 18:22:20 -0800

    Added models to store our data.

---
+++ app/Widgets/TopSocialPlatforms/Migrations/2021_01_23_020623_create_platform_stats_table.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * This file is part of Digital Atlas.
+ *
+ * Digital Atlas is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Digital Atlas is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @license http://opensource.org/licenses/gpl-license.php GNU Public License
+ *
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePlatformStatsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('platform_stats', function (Blueprint $table) {
+            $table->id();
+            $table->decimal('percentage', $precision = 5, $scale = 2);
+            $table->smallInteger('month_reported');
+            $table->smallInteger('year_reported');
+            $table->timestamp('created_at')->useCurrent();
+        });
+        Schema::table('platform_stats', function (Blueprint $table) {
+            $table->foreignId('social_platform_id')->constrained()->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('platform_stats');
+    }
+}
