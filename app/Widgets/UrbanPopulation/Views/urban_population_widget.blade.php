f5379bb27427088b0b64356c7ebbfa36f28b3f18
LeviCostello <46667507+LeviCostello@users.noreply.github.com>
2021-02-16 16:27:46 -0800

    adding urban population widget (#25)
    * adding urban population widget
    
    * adding urban pop
    
    * final fixes for urban population
    
    Co-authored-by: LeviCostello <leviwcostello@gmail.com>

---
+++ app/Widgets/UrbanPopulation/Views/urban_population_widget.blade.php
@@ -0,0 +1,73 @@
+<div class="card m-3">
+    @if(!$current->exists())
+        <div class="card-body">
+            <h3 class="card-title">{{ ucwords(trans('urban-population::widget.title')) }}</h3>
+            <p>{{ trans('urban-population:widget.missing_data') }}</p>
+        </div>
+    @else
+        <canvas id="urban-population-chart" class="card-img-top" height="400" styles="width: 100%; height: auto;"></canvas>
+        <div class="card-body">
+            <h3 class="card-title">{{ $current->year_reported }} {{ ucfirst(trans('urban-population::widget.title')) }}</h3>
+            <dl class="row">
+                <dt class="col-sm-5">{{ ucfirst(trans('urban-population::widget.total')) }}</dt>
+                <dd class="col-sm-7">{{ number_format($current->total, 3) }}%</dd>
+            </dl>
+        </div>
+    @endif
+    <div class="card-footer text-muted">
+        <p class="credit">{{ ucfirst(trans('urban-population::widget.provided_by')) }} <a href="https://data.worldbank.org/indicator/SP.URB.TOTL.IN.ZS" target="_blank" rel="nofollow">{{ trans('urban-population::widget.provider') }}</a></p>
+    </div>
+</div>
+<script type="text/javascript">
+$(function() {
+@if($current->exists())
+    var urbanPopulationChart = $('#urban-population-chart');
+    new Chart(urbanPopulationChart, {
+        "type": "line",
+        "data": {
+            "labels": @json($statLabels),
+            "datasets": [{
+                "label":  "{{ ucfirst(trans('urban-population::widget.title')) }}",
+                "data": @json($statData),
+                "fill": false,
+                "borderColor": "{{$lineColor}}",
+                "lineTension": 0.1
+            }]
+        },
+        "options": {
+            "tooltips": {
+                "callbacks": {
+                    "label": function(tooltipItems, data) {
+                        return data.labels[tooltipItems.index] + " " + data.datasets[0].data[tooltipItems.index].toLocaleString();
+                    }
+                }
+            },
+            "scales": {
+                "xAxes": [
+                    {
+                        "scaleLabel": {
+                            "display": true,
+                            "labelString": "{{ ucwords(trans('urban-population::widget.year')) }}"
+                        }
+                    }
+                ],
+                "yAxes": [
+                    {
+                        "scaleLabel": {
+                            "display": true,
+                            "labelString": "{{ ucwords(trans('urban-population::widget.xaxis')) }}"
+                        },
+                        "ticks": {
+                            "beginAtZero": false,
+                            "callback": function(value, index, values) {
+                              return value.toLocaleString();
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    });
+@endif
+});
+</script>
