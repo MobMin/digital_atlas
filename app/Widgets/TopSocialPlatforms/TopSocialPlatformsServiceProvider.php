4787d83a179ba5e96dea1c2610030ad30608abad
Codemis <johnathan@pulos.com>
2021-04-14 11:43:56 -0700

    Added CRON task.

--- app/Widgets/TopSocialPlatforms/TopSocialPlatformsServiceProvider.php
+++ app/Widgets/TopSocialPlatforms/TopSocialPlatformsServiceProvider.php
@@ -48,7 +48,7 @@ class TopSocialPlatformsServiceProvider extends ServiceProvider
             __DIR__  . $ds . 'Config.php' => config_path('widgets' . $ds . 'top_social_platforms.php'),
         ]);
         $this->callAfterResolving(Schedule::class, function (Schedule $schedule) {
-            // Schedule your cron
+            $schedule->command('import:top-social-stats')->monthly();
         });
         if ($this->app->runningInConsole()) {
             // Add commands for artisan here
