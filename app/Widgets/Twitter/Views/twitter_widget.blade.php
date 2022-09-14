0ae373c43ff53355e8fe56f1c15a407a127358f7
Codemis <johnathan@pulos.com>
2022-09-14 10:29:22 -0700

    Fixed an issue where hashtags were not properly encoded.

--- app/Widgets/Twitter/Views/twitter_widget.blade.php
+++ app/Widgets/Twitter/Views/twitter_widget.blade.php
@@ -12,7 +12,7 @@
             <tbody>
                 @foreach($tweets as $val)
                     <tr>
-                        <td><a href="http://twitter.com/search?q={{$val->tweet_name}}" target="_blank">{{$val->tweet_name}}</a></td>
+                        <td><a href="http://twitter.com/search?q={{urlencode($val->tweet_name)}}" target="_blank">{{$val->tweet_name}}</a></td>
                         @if($val->tweet_count > 0)
                             <td>{{number_format($val->tweet_count)}}</td>
                         @else
