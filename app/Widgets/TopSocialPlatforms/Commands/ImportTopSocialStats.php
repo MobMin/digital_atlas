e39f9891ffa0a95c97a952b02e24ba7811d55e76
Codemis <johnathan@pulos.com>
2022-11-11 10:47:21 -0800

    Added a try catch so the script does not die when 1 stat fails.

--- app/Widgets/TopSocialPlatforms/Commands/ImportTopSocialStats.php
+++ app/Widgets/TopSocialPlatforms/Commands/ImportTopSocialStats.php
@@ -92,7 +92,12 @@ class ImportTopSocialStats extends Command
         foreach ($countries as $code => $id) {
             $this->info('Importing ' . $code . ' stats.');
             $statUrl = $this->getUrl($code);
-            $fileData = fopen($statUrl, 'r');
+            try {
+                $fileData = fopen($statUrl, 'r');
+            } catch (Exception $e) {
+                $this->error('Unable to retrieve file for ' . $code . '.');
+                continue;
+            }
             $count = 0;
             $data = [];
             while (($line = fgetcsv($fileData)) !== false) {
