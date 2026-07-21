cb4b77639924e098c13f0f0102ecfddb3f0646c2
Codemis <johnathan@jpulos.com>
2026-07-20 18:32:15 -0700

    Cleaned up the code.

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -25,6 +25,8 @@ namespace App\Http\Controllers;
 
 use App\Models\Country;
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
+use Illuminate\View\View;
 
 /**
  * Our Country controller
@@ -34,9 +36,9 @@ class CountryController extends Controller
     /**
      * Show all countries
      *
-     * @return Response
+     * @return View
      */
-    public function index()
+    public function index(): View
     {
         $photos = config('home_photos');
         $random = array_rand($photos);
@@ -47,9 +49,9 @@ class CountryController extends Controller
      * Show a specific country
      *
      * @param  string $slug The country slug
-     * @return Response
+     * @return View
      */
-    public function show($slug)
+    public function show(string $slug): View
     {
         $country = Country::where('slug', $slug)->first();
         if ($country == null) {
@@ -62,16 +64,13 @@ class CountryController extends Controller
      * Fetch all the countries for a specific query
      *
      * @param  Request $request The request object
-     * @return Response         The JSON response
+     * @return JsonResponse         The JSON response
      */
-    public function fetch(Request $request)
+    public function fetch(Request $request): JsonResponse
     {
-        $query = $request->get('query');
-        if ($query) {
-            $countries = Country::where('name', 'LIKE', "{$query}%")->get();
-        } else {
-            $countries = Country::get();
-        }
+        $query = $request->input('query', '');
+        $countries = ($query) ? Country::where('name', 'LIKE', "{$query}%")->get() : Country::get();
+
         return response()->json($countries);
     }
 }
