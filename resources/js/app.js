0644f46ae85ab38753d5d4544bca7d1ff175cfd1
Codemis <johnathan@jpulos.com>
2026-07-20 18:55:57 -0700

    Removed JQuery backstretch. We now use plain CSS.

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,6 @@ import Chart from 'chart.js/auto';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import '@fortawesome/fontawesome-free/css/all.css';
-import 'jquery-backstretch';
 
 window.Chart = Chart;
 window.L = L;
--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
+                'resources/css/app.css',
                 'resources/js/app.js',
                 'resources/js/filter-widgets.js',
                 'resources/sass/app.scss',
